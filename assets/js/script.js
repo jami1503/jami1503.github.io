1640697dfe69edaf5489fee2a25b5c52576f64dd
José Alejandro Malavé <lord_alejandro_741@hotmail.com>
2023-04-07 14:27:04 -0400

    Botón de sroll top

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/*Botón de ir al tope de la página*/
+document.getElementById("top-button").addEventListener("click", scrollUp)
+
+function scrollUp() {
+    var currentScroll = document.documentElement.scrollTo;
+    if (currentScroll > 0) {
+        window.scrollTo (0, 0);
+    }
+}
