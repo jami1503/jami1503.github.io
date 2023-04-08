fd9ab9a26c0345fc24263ce8ff80cd24643a23f7
jmmalave <jmmalave19@gmail.com>
2023-04-08 11:52:33 -0500

    Se añadio la funcionalidad scroll to up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
-/*Botón de ir al tope de la página*/
+$(document).ready(function() {
 
-()
+  // Variables para el botón y el elemento de destino
+  var $buttonUp = $('.button-up');
+  var $contentTable = $('.content-table');
 
+  // Oculta el botón al principio
+  $buttonUp.hide();
 
-document.getElementById("top-button").addEventListener("click", scrollUp)
-
-function scrollUp() {
-    var currentScroll = document.documentElement.scrollTo;
-    if (currentScroll > 0) {
-        window.scrollTo (0, 0);
+  // Muestra el botón al desplazarse hacia abajo
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > ($(window).height() + 400) / 2) {
+      $buttonUp.fadeIn();
+    } else {
+      $buttonUp.fadeOut();
     }
-}
+  });
+
+  // Desplaza suavemente hacia arriba al hacer clic en el botón
+  $buttonUp.click(function() {
+    $('html, body').animate({scrollTop: $contentTable.offset().top}, 800);
+    return false;
+  });
+
+});
