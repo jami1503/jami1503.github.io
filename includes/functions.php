1408ac54d33b8a855ea07f3b8230add8475f7d42
Jose Miguel <32186977+jmmalave@users.noreply.github.com>
2023-04-16 08:36:12 -0500

    update includes/functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -12,17 +12,17 @@ function openDocument(){
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title><?= $page_title; ?></title>
-        <link rel="stylesheet" href="./assets/css/style.css" />
-        <link rel="icon" type="image/x-icon" href="/assets/img/logo.png" />
-	    <!-- Google font icons -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-	</head>
-	<body>
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title><?= $page_title; ?></title>
+	<link rel="stylesheet" href="./assets/css/style.css" />
+	<link rel="icon" type="image/x-icon" href="/assets/img/logo.png" />
+	<!-- Google font icons -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+</head>
+<body>
 <?php } ?>
 
 <?php
@@ -33,13 +33,13 @@ function openDocument(){
 
 
 function closeDocument(){ ?>
-	<button type="button" class="top-button">
-		<span class="material-symbols-outlined">
-			expand_less 
-		</span>
-	</button>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="/assets/js/script.js"></script>
+<button type="button" class="top-button">
+	<span class="material-symbols-outlined">
+		expand_less 
+	</span>
+</button>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="/assets/js/script.js"></script>
 </body>
 </html>
 <?php } ?>
@@ -52,10 +52,53 @@ function closeDocument(){ ?>
  
 function getHeader(){ ?>
 <header class="page-header">
-    <div class="brand">
-        <a href="/">
-          <img src="/assets/img/logo.png" class="logo" />
-        </a>
-    </div>
+	<div class="brand">
+		<a href="/">
+			<img src="/assets/img/logo.png" class="logo" />
+		</a>
+	</div>
+	
+	<!-- menu -->
+	<nav class="menu">
+		<ul class="nav">
+			<li class="nav-item">
+				<a href="/" class="nav-link">
+					Inicio
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="/#about" class="nav-link">
+					Nosotros
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="/contacto.php" class="nav-link">
+					Contacto
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="/" class="nav-link">
+					Chamet
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="https://h5.ichamet.com/webH5/agent/bind.html?companyId=55309" class="nav-link cta-link">
+					Unirse
+				</a>
+			</li>
+		</ul>
+	</nav>
+	
+	<!-- botón qué despliega el menú responsivo -->
+	<button class="btn-toggle-menu btn-sow-menu">
+		<span class="material-symbols-outlined">
+			menu
+		</span> 
+	</button>
+	
+	
+	
+	<!-- menu responsivo -- >
+	
 </header>
 <?php } ?>
