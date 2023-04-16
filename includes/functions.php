be3f45c03b6dcf4d916ad06138b6f778adebbaa7
jmmalave19@gmail.com <jmmalave19@gmail.com>
2023-04-15 23:14:00 -0500

    update file /includes/functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,39 +1,61 @@
 <?php
 
+/**
+ * function openDocument 
+ * @var String global $page_title
+ */
+
 function openDocument(){
 	
 	global $page_title;
 	
-	echo "
-		<!DOCTYPE html>
-		<html lang=\"es\">
-		  <head>
-		    <meta charset=\"UTF-8\" />
-		    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />
-		    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
-		    <title>$page_title</title>
-			<title>Stream Queens Agency</title>
-		    <link rel=\"stylesheet\" href=\"./assets/css/style.css\" />
-		    <link
-		      rel=\"icon\"
-		      type=\"image/x-icon\"
-		      href=\"./assets/img/SQ_logo-removebg-preview (1).png\"
-		    />
-		    <!-- Google font icons -->
-		    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0\" />
-		  </head>
-		  <body>";
-}
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title><?= $page_title; ?></title>
+        <link rel="stylesheet" href="./assets/css/style.css" />
+        <link rel="icon" type="image/x-icon" href="/assets/img/logo.png" />
+	    <!-- Google font icons -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+	</head>
+	<body>
+<?php } ?>
 
-function closeDocument(){
-	echo "
-	<button type=\"button\" class=\"top-button\">
-		<span class=\"material-symbols-outlined\">
+<?php
+
+/**
+ * function closeDocument 
+ */
+
+
+function closeDocument(){ ?>
+	<button type="button" class="top-button">
+		<span class="material-symbols-outlined">
 			expand_less 
 		</span>
 	</button>
-	<script src=\"https://code.jquery.com/jquery-3.6.0.min.js\"></script>
-    <script src=\"/assets/js/script.js\"></script>
-    </body>
-	</html>";
-}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/assets/js/script.js"></script>
+</body>
+</html>
+<?php } ?>
+
+<?php
+
+/**
+ * function getHeader
+ */
+ 
+function getHeader(){ ?>
+<header class="page-header">
+    <div class="brand">
+        <a href="/">
+          <img src="/assets/img/logo.png" class="logo" />
+        </a>
+    </div>
+</header>
+<?php } ?>
